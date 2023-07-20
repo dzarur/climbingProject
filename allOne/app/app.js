5fb5cfa139d601578d234269f82143a3c230a57f
Daniel Zarur <zarurdaniel@gmail.com>
2023-07-20 12:09:11 -0700

    progress

--- allOne/app/app.js
+++ allOne/app/app.js
@@ -1,9 +1,22 @@
-import { Button, Text, View, StyleSheet, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './Animation';
+import LoginScreen from './logIn'
+import SignUpScreen from './SignUpScreen';
+import AppScreen from './app'
 
-export default function AppScreen() {
-    return(
-        <View>
-            <Text>Hello and Welcome to our app</Text>
-        </View>
-    )
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Log In" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="App" component={AppScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App;
---
+++ allOne/app/something.js
@@ -0,0 +1,9 @@
+import { Button, Text, View, StyleSheet, } from 'react-native';
+
+export default function AppScreen() {
+    return(
+        <View>
+            <Text>Hello and Welcome to our app</Text>
+        </View>
+    )
+}
